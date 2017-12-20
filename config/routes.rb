2e2871b71bf9383e272e3eda031afab94ac5ad2c
Cristian Barahona G <cristianvirtual@gmail.com>
2017-12-20 16:35:06 -0300

    omniauth with facebook

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   get 'home/index'
 
-  devise_for :users
+  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
+
   authenticated :user do
     root 'home#index', as: 'authenticated_root'
   end
