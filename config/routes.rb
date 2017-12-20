46c9d31d90c843ac87cfb0ce12863759d782a897
Pablo Toro <ptoro@grouponlatam.com>
2017-12-20 14:07:17 -0300

    login facebook

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  get 'home/index'
+
   devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  authenticated :user do
+    root 'home#index', as: 'authenticated_root'
+  end
+  devise_scope :user do
+    root 'devise/sessions#new'
+  end
 end
