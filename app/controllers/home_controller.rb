5ad531ddd06d037e6879f00447ee58c026a3a634
Cristian Barahona G <cristianvirtual@gmail.com>
2018-02-25 20:20:10 -0300

    minor fixes

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,5 @@
 class HomeController < ApplicationController
   def index
-<<<<<<< HEAD
-  	@trips = Trip.where(user_id: 1) 
-=======
     @trips = Trip.where(user_id: 1)
->>>>>>> add home view
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,21 @@
 Rails.application.routes.draw do
+  root to: 'home#index'
+
+  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
+
+  # authenticated :user do
+  #   root 'home#index', as: 'authenticated_root'
+  # end
+  # devise_scope :user do
+  #   root 'devise/sessions#new'
+  # end
+
   resources :books, param: :code do
     resources :trips
   end
   resources :genres
   resources :publishers
   resources :authors
-  get 'home/index'
 
-  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
 
-  authenticated :user do
-    root 'home#index', as: 'authenticated_root'
-  end
-  devise_scope :user do
-    root 'devise/sessions#new'
-  end
 end
